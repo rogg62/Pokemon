c1c34ad5510c635ff6e20125e0e1de8823cd9666
Rodrigues <rdgs2309@gmail.com>
2023-08-12 16:40:45 -0300

    ...

--- src/components/cards/Card.js
+++ src/components/cards/Card.js
@@ -1,25 +1,26 @@
 import React from "react";
-import CardImg from "./cardImagem/CardImg";
 import Card from "react-bootstrap/Card";
-
 import "./style.css";
-const Cards = ({ name }) => {
+const Cards = ({ name, img, indice }) => {
   return (
     <>
-      {name &&
-        name.results.map((i) => (
-          <Card
-            className="d-flex align-items-center justify-content-center gap-0 position-relative rounded"
-            style={{ width: "177px", height: "115.178px" }}
-            key={i.name}
+      <Card
+        className="d-flex align-items-center justify-content-center gap-0 position-relative"
+        style={{ width: "177px", height: "115.178px" }}
+      >
+        <img src={img} id="img" alt="" />
+
+        <div id="id">{indice}</div>
+        <Card.Body className=" m-0 d-flex flex-column align-items-center justify-content-end">
+          <Card.Title
+            id="title"
+            className="m-0 d-flex flex-row-reverse align-items-center justify-content-center gap-3"
           >
-            <CardImg pokemon={i.name} />
-            <Card.Body className=" m-0 d-flex flex-column align-items-center justify-content-end">
-              <Card.Title className="m-0">{i.name}</Card.Title>
-              <Card.Text className="d-flex justify-content-around"></Card.Text>
-            </Card.Body>
-          </Card>
-        ))}
+            {name}
+          </Card.Title>
+          <Card.Text className="d-flex justify-content-around"></Card.Text>
+        </Card.Body>
+      </Card>
     </>
   );
 };
--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,27 +1,63 @@
-import React, { useEffect, useState } from "react";
 import Container from "react-bootstrap/Container";
 import Cards from "../cards/Card";
-const Main = () => {
-  const [name, setName] = useState();
+import axios from "axios";
 
+import { useEffect, useState } from "react";
+const Main = () => {
+  const [pokemons, setPokemons] = useState([]);
   useEffect(() => {
-    async function pegarName() {
-      const res = await fetch("https://pokeapi.co/api/v2/pokemon?limit=151");
-      const data = await res.json();
-      setName(data);
-    }
-    pegarName();
+    getPokemons();
   }, []);
 
+  const getPokemons = () => {
+    var endpoints = [];
+    for (var i = 1; i < 152; i++) {
+      endpoints.push(`https://pokeapi.co/api/v2/pokemon/${i}/`);
+    }
+    axios
+      .all(endpoints.map((endpoint) => axios.get(endpoint)))
+      .then((res) => setPokemons(res));
+  };
+
+  const pokemonFilter = (name) => {
+    var filteredPokemons = [];
+    if (name === "") {
+      getPokemons();
+    }
+    for (var i in pokemons) {
+      if (pokemons[i].data.name.includes(name)) {
+        filteredPokemons.push(pokemons[i]);
+      }
+    }
+
+    setPokemons(filteredPokemons);
+  };
+
   return (
-    <main
-      style={{ width: "100%", height: "auto", background: "#fff" }}
-      className="bg-body-secondary p-3"
-    >
-      <Container className="d-flex flex-wrap align-items-center justify-content-around gap-4">
-        <Cards name={name} />
-      </Container>
-    </main>
+    <>
+      <main
+        style={{ width: "100%", height: "auto", background: "#fff" }}
+        className="bg-body-secondary d-flex flex-column gap-5"
+      >
+        <div className="bg-white d-flex justify-content-end">
+          <input
+            className="btn btn-dark mr-3"
+            type="search"
+            onChange={(e) => pokemonFilter(e.target.value)}
+          />
+        </div>
+        <Container className="d-flex flex-wrap align-items-center justify-content-around gap-4">
+          {pokemons.map((pokemon, key) => (
+            <Cards
+              name={pokemon.data.name}
+              img={pokemon.data.sprites.front_default}
+              indice={pokemon.data.id}
+              key={key}
+            />
+          ))}
+        </Container>
+      </main>
+    </>
   );
 };
 
